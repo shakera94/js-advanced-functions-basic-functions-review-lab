530f5b2dd4f61b169ca047eee5346a01706f14f0
shakera <sanderso_4516574@yahoo.com>
2021-04-04 16:26:32 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+ return (`This Saturday, I want to ${activity}!`)
+}
+function mondayWork(activity = "go to the office") {
+    return (`This Monday, I will ${activity}.`)
+}
+let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  } 
+ let Calculator = {
+   add: function(num1, num2) {
+     return num1 + num2},
+     subtract: function(num1, num2){
+      return num1 - num2}, 
+    multiply: function(num1, num2){
+    return num1 * num2},
+   divide: function(num1, num2){ 
+    return num1 / num2}
+ }
+
+ let actionApplyer = function(start, ray) {
+  let a = start
+
+  for (let i = 0; i < ray.length; i++ ){
+    a = ray[i](a)}
+    return a
+  }
